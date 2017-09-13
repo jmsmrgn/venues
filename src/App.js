f3ff39daaa90e94fd88cf3c5b86838eec9451900
jmsmrgn <contact@jamesmorgan.io>
2017-09-13 16:50:31 -0700

    add header and venue components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'normalize.css'
+import './app.css'
+import styled from 'styled-components'
+import Header from './components/header/header'
+import Venue from './components/venue/venue'
+
+const VenueContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 1440px;
+  margin: 0 auto;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>SF Tunage</h2>
-        </div>
-        <p className="App-intro">
-          unified event calendar for select venues in san francisco - pulls from songkick's api
-        </p>
+        <Header title="SF Venues" />
+        <VenueContainer>
+          <Venue
+            venueName="Rickshaw Stop"
+            venueID="14791-rickshaw-stop"
+            venueUrl="http://www.rickshawstop.com/"
+          />
+          <Venue
+            venueName="Bottom of the Hill"
+            venueID="248-bottom-of-the-hill"
+            venueUrl="http://www.bottomofthehill.com/"
+          />
+          <Venue
+            venueName="1015 Folsom"
+            venueID="5670-1015-folsom"
+            venueUrl="http://www.1015.com"
+          />
+          <Venue
+            venueName="Audio"
+            venueID="2393199-audio"
+            venueUrl="http://www.audiosf.com/"
+          />
+          <Venue
+            venueName="Fox Theater"
+            venueID="953251-fox-theater"
+            venueUrl="http://www.thefoxoakland.com/"
+          />
+          <Venue
+            venueName="The Independent"
+            venueID="324-independent"
+            venueUrl="http://www.theindependentsf.com/"
+          />
+          <Venue
+            venueName="The Mezzanine"
+            venueID="329-mezzanine"
+            venueUrl="http://www.mezzaninesf.com/"
+          />
+          <Venue
+            venueName="Hotel Utah Saloon"
+            venueID="328-hotel-utah-saloon"
+            venueUrl="http://www.hotelutah.com/"
+          />
+          <Venue
+            venueName="The Chapel"
+            venueID="2029634-chapel"
+            venueUrl="http://www.thechapelsf.com/"
+          />
+          <Venue
+            venueName="Slim's"
+            venueID="1489-slims"
+            venueUrl="http://www.slims-sf.com/"
+          />
+        </VenueContainer>
       </div>
     );
   }
---
+++ src/components/venue/venue.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding: 5px;
+  width: 100%;
+  @media (min-width: 500px) {
+    width: 50%;
+  }
+  @media (min-width: 767px) {
+    width: 33.33%;
+  }
+  @media (min-width: 1024px) {
+    width: 25%;
+  }
+  @media (min-width: 1600px) {
+    width: 20%;
+  }
+`
+
+const Button = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  width: 100%;
+  padding: 20px;
+  background: oldlace;
+  color: mediumaquamarine;
+  text-decoration: none;
+  :hover {
+    cursor: pointer;
+    margin-top: -1px;
+  }
+  a {
+    color: white;
+    font-weight: normal;
+    display: block;
+    background: burlywood;
+    text-decoration: none;
+    margin: 10px auto 0 auto;
+    width: 80px;
+    &:hover {
+      background-color: navajowhite;
+    }
+  }
+`
+
+const List = styled.ul`
+  background: mediumaquamarine;
+  list-style-type: none;
+  text-align: left;
+  padding: 5px 10px;
+  margin: 0;
+  li {
+    color: papayawhip;
+    font-size: 11px;
+    margin-bottom: 2px;
+  }
+`
+
+const ListItem = styled.li`
+  display: flex;
+  align-content: flex-start;
+`
+
+const DayOfWeek = styled.span`
+  width: 65px;
+  margin-right: 5px;
+  font-size: 12px;
+`
+
+const DayOfMonth = styled.span`
+  color: firebrick;
+  width: 40px;
+  margin-right: 5px;
+  font-size: 12px;
+`
+
+
+
+class Venue extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [],
+      fetched: false,
+      showList: false
+    }
+    this.fetchVenue = this.fetchVenue.bind(this)
+  }
+
+  async fetchVenue() {
+    if (this.state.fetched) {
+      this.setState({ showList: !this.state.showList })
+      return
+    }
+
+    const venueID = this.props.venueID
+    const res = await axios.get(`http://api.songkick.com/api/3.0/venues/${venueID}/calendar.json?apikey=Ap6UKNWuYTXt70qi`)
+
+    this.setState({
+      data: res.data.resultsPage.results.event,
+      fetched: true,
+      showList: true
+    })
+  }
+
+  getDayOfWeek(date) {
+    const dayOfWeek = new Date(date).getUTCDay()
+    return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+  }
+
+  render() {
+    const renderList = this.state.data.map((item, key) => {
+      const dayOfWeek = this.getDayOfWeek(item.start.date)
+      const date = item.start.date
+      const dateShort = date.substring(date.indexOf('-') + 1);
+
+      return (
+        <ListItem key={key}>
+          <DayOfMonth>{dateShort}</DayOfMonth>
+          <DayOfWeek>{dayOfWeek}</DayOfWeek>
+          <a href={item.uri} target="_blank">{item.performance[0].artist.displayName}</a>
+        </ListItem>
+      )
+    })
+
+    return (
+      <Container>
+        <Button onClick={this.fetchVenue} venueID={this.props.value}>
+          {this.props.venueName}
+          <a href={this.props.venueUrl} target="_blank" onClick={(e) => e.stopPropagation()}>website</a>
+        </Button>
+        {
+          this.state.showList ? <List>{renderList}</List> : null
+        }
+      </Container>
+    )
+  }
+}
+
+export default Venue
