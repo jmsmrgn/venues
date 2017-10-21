c062efb03eca4b21ac1353ea3b9710fe85c0c1f5
jmsmrgn <contact@jamesmorgan.io>
2017-10-21 10:54:36 -0700

    Catch axios errors

--- src/components/venue-card/venue-card.js
+++ src/components/venue-card/venue-card.js
@@ -30,10 +30,18 @@ class VenueCard extends Component {
     let res
 
     if (this.props.songkick) {
-      res = await axios.get(`https://api.songkick.com/api/3.0/venues/${venueID}/calendar.json?apikey=${process.env.REACT_APP_SONGKICK_API_KEY}`)
+      res = await axios
+        .get(`https://api.songkick.com/api/3.0/venues/${venueID}/calendar.json?apikey=${process.env.REACT_APP_SONGKICK_API_KEY}`)
+        .catch(err => {
+          console.log(err)
+        })
     }
     if (this.props.ticketmaster) {
-      res = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?venueId=${venueID}&size=100&apikey=${process.env.REACT_APP_TICKETMASTER_API_KEY}`)
+      res = await axios
+        .get(`https://app.ticketmaster.com/discovery/v2/events.json?venueId=${venueID}&size=100&apikey=${process.env.REACT_APP_TICKETMASTER_API_KEY}`)
+        .catch(err => {
+          console.log(err)
+        })
     }
 
     const data = this.props.songkick ? res.data.resultsPage.results.event : res.data._embedded.events
